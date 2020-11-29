cbf4ad25ce5b03ad627fd0eadacf6f7bdbbf8c0c
David Trigueiros <david.ordaz96@gmail.com>
2020-11-29 21:08:39 +0000

    Add project files.

---
+++ STT_Bot/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace STT_Bot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.LstCommands = new System.Windows.Forms.ListBox();
+            this.TimerSpeaking = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // LstCommands
+            // 
+            this.LstCommands.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.LstCommands.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LstCommands.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.LstCommands.ForeColor = System.Drawing.SystemColors.Control;
+            this.LstCommands.FormattingEnabled = true;
+            this.LstCommands.ItemHeight = 20;
+            this.LstCommands.Location = new System.Drawing.Point(0, 0);
+            this.LstCommands.Name = "LstCommands";
+            this.LstCommands.Size = new System.Drawing.Size(597, 375);
+            this.LstCommands.TabIndex = 0;
+            this.LstCommands.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // TimerSpeaking
+            // 
+            this.TimerSpeaking.Enabled = true;
+            this.TimerSpeaking.Interval = 1000;
+            this.TimerSpeaking.Tick += new System.EventHandler(this.TimerSpeaking_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(597, 375);
+            this.Controls.Add(this.LstCommands);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox LstCommands;
+        private System.Windows.Forms.Timer TimerSpeaking;
+    }
+}
+
---
+++ STT_Bot/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+
+namespace STT_Bot
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine _recognizer = new SpeechRecognitionEngine();
+        SpeechSynthesizer Bot = new SpeechSynthesizer();
+        SpeechRecognitionEngine startlistening = new SpeechRecognitionEngine();
+        Random rnd = new Random();
+        int RecTimeOut = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _recognizer.SetInputToDefaultAudioDevice();
+            _recognizer.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"DefaultCommands.txt")))));
+            _recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Default_SpeechRecognized);
+            _recognizer.SpeechDetected += new EventHandler<SpeechDetectedEventArgs>(_recognizer_SpeechRecognized);
+            _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+            startlistening.SetInputToDefaultAudioDevice();
+            startlistening.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"DefaultCommands.txt")))));
+            startlistening.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(startlistening_SpeechRecognized);
+
+        }
+
+        private void Default_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            int ranNum;
+            string speech = e.Result.Text;
+
+            if (speech == "Hello")
+            {
+                Bot.SpeakAsync("Hello, I am here");
+            }
+            if (speech == "How are you")
+            {
+                Bot.SpeakAsync("I am working normally");
+            }
+            if (speech == "What time is it")
+            {
+                Bot.SpeakAsync(DateTime.Now.ToString("h mm tt"));
+            }
+            if (speech == "Stop talking")
+            {
+                Bot.SpeakAsyncCancelAll();
+                ranNum = rnd.Next(1, 2);
+                if (ranNum == 1)
+                {
+                    Bot.SpeakAsync("Yes sir");
+                }
+                else if (ranNum == 2)
+                {
+                    Bot.SpeakAsync("I will be quiet");
+                }
+            }
+            if (speech == "Stop listening")
+            {
+                Bot.SpeakAsync("if you need me just ask");
+                _recognizer.RecognizeAsyncCancel();
+                startlistening.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            if (speech == "Show commands")
+            {
+                string[] commands = (File.ReadAllLines(@"DefaultCommands.txt"));
+                LstCommands.Items.Clear();
+                LstCommands.SelectionMode = SelectionMode.None;
+                LstCommands.Visible = true;
+                foreach(string command in commands)
+                {
+                    LstCommands.Items.Add(command);
+                }
+            }
+            if (speech == "Hide commands")
+            {
+                LstCommands.Visible = false;
+            }
+        }
+
+        private void _recognizer_SpeechRecognized(object sender, SpeechDetectedEventArgs e)
+        {
+            RecTimeOut = 0;
+        }
+
+        private void startlistening_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string speech = e.Result.Text;
+            
+            if(speech == "Wake up")
+            {
+                startlistening.RecognizeAsyncCancel();
+                Bot.SpeakAsync("Yes, I am here");
+                _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            }
+        }
+
+        private void TimerSpeaking_Tick(object sender, EventArgs e)
+        {
+            if (RecTimeOut == 10)
+            {
+                _recognizer.RecognizeAsyncCancel();
+            }
+            else if (RecTimeOut == 11)
+            {
+                TimerSpeaking.Stop();
+                startlistening.RecognizeAsync(RecognizeMode.Multiple);
+                RecTimeOut = 0;
+            }
+        }
+    }
+}
